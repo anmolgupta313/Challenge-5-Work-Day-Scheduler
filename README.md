1318cb2d721052bd11546ad7ffa2d8c08f2eb2df
anmolgupta313 <111723339+anmolgupta313@users.noreply.github.com>
2022-10-03 23:55:03 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Challenge-5-Work-Day-Scheduler
+
+# Web-ApI-Quiz-Chaallenge
+
+# Password-Generater-Challenge
+
+## Description Of the Challenge
+
+This is the 5th week challenge that were given to us by the University Of Toronto in the coding bootcamp.
+In this challenge we have been asked to complete the starter code which purpose is to create a workday schedule and store value in the local storage 
+
+## Completed
+by following all the instructions I am able to done this task and in this task I have completed the code by writing the Html, Css & js.
+
+## Result
+
+<img width="1470" alt="Screen Shot 2022-10-03 at 11 46 30 PM" src="https://user-images.githubusercontent.com/111723339/193730283-8f828638-97c1-4760-abca-68ec83dad261.png">
+
+
+
+
+## Link of the Web Page: 
+https://anmolgupta313.github.io/Password-Generator-Challenge/
+
+## License
+
+Please refer to the LICENSE in the repo.
---
+++ script.js
@@ -0,0 +1,52 @@
+//created variable in which we have targeted current day
+var displayCurrentDay= document.querySelector("#currentDay");
+
+//Created variable and added moment.
+var currentDay= moment();
+
+//added date formar to display current
+displayCurrentDay.innerHTML= currentDay.format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+//adding class to time block class
+var timeBlock = $(".time-block").addClass("row");
+
+var currentHour = 11 //parseInt(moment().format("H"));
+
+
+//selected save button and created an array and created a loop plus set get item
+var saveBtn= document.getElementsByClassName("saveBtn");
+var timeSlots=["9","10","11","12", "13", "14", "15", "16", "17"];
+for(var i =0; i<timeSlots.length; i++){
+    const slot= timeSlots[i];
+    var textArea= document.getElementById(slot);
+    textArea.innerHTML= localStorage.getItem(slot);
+}
+
+// time blocks COLOR CODED to indicate past, present, or future
+$("textarea").each(function () {
+    var $this = $(this);
+    var id = parseInt($this.attr("id"));
+  
+    if (currentHour > id) {
+      $(this).addClass("past");
+    }
+    if (currentHour < id) {
+      $(this).addClass("future");
+    }
+    if (currentHour === id) {
+      $(this).addClass("present");
+    }
+  });
+
+// calling an event listner on each save button and setting value in local storage.
+Object.values(saveBtn).forEach(saveBtn => {
+    saveBtn.addEventListener("click", function(event){
+        const id = event.target.id;
+        const key =  id.split("_")[1];
+        let textArea= document.getElementById(key).value; 
+        localStorage.setItem(key, textArea);
+    
+    })    
+});
+
+
